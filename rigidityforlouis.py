313d1f94f4ae95761a5941fb0d1e2d3a3156f95b
jsouthgate1 <32932257+jsouthgate1@users.noreply.github.com>
2021-07-18 20:38:48 +0100

    Create rigidityforlouis.py

---
+++ rigidityforlouis.py
@@ -0,0 +1,74 @@
+def rigmat2(E,P): #I got an error using the matrix_from_rows_and_columns property so I had to improvise the cross product, need to find a way around this for higher dimensions theough
+	n = P.ncols()
+	m = len(E)
+	Prows = P.rows()
+	R = []
+	for e in E:
+		MatRow = []
+		for i in range(n):
+			for coord in range(2):
+				if i not in e:
+					e.append(0)
+				else:
+					for projn in range(2):
+						if projn != coord:
+							L = projn
+					OppEdge = []
+					for j in e:
+						if j != i:
+							OppEdge.apend(j)
+					minor = P[L,OppEdge[0]] - P[L,OppEdge[1]]
+					if (coord == 0 and i > OppEdge[0] and i < OppEdge[1]) or (coord == 1 and i < OppEdge[0] and i < OppEdge[1]) or (coord == 1 and i > OppEdge[0] and i > OppEdge[1]):
+						MatRow.append(-minor)
+					else:
+						MatRow.append(minor)
+		R.append(MatRow)
+	return(matrix(R))
+
+def stressmat2(E,n,stress): # When checking this make sure the matrix of distance vectors is indexed by OneSkel
+	OneSkel = []	
+	OneSkelReps = []
+	Stress = []
+	for e in E:
+		Subs = Subsets(e,2)
+		for i in Subs:
+			OneSkelReps.append(list(i))
+	for i in OneSkelReps:
+		if i not in OneSkel:
+			OneSkel.append(i)
+	for i in range(n):
+		MatRow = []
+		for OppEdge in OneSkel:
+			L0 = []
+			L1 = []
+			L2 = []
+			for j in OppEdge:
+				L0.append(j)
+				L1.append(j)
+				L2.append(j)
+			if i in OppEdge:
+				MatRow.append(0)
+			else:
+				L0.insert(0,i)
+				L1.insert(1,i)
+				L2.insert(2,i)
+				if L0 in E:
+					MatRow.append(stress[E.index(L0)])
+				elif L1 in E:
+					MatRow.append(-stress[E.index(L1)])
+				elif L2 in E:
+					MatRow.append(stress[E.index(L2)])
+				else:
+					MatRow.append(0)
+		Stress.append(MatRow)
+	return([matrix(Stress),OneSkel])
+
+def distmat2(P,OneSkel):
+	s = len(OneSkel)
+	D = []
+	for coord in range(2):
+		DRow = []
+		for Edge in OneSkel:
+			DRow.append(P[coord,Edge[1]]-P[coord,Edge[0]])
+		D.append(DRow)
+	return(matrix(D))
